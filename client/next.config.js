227c5b7f849551b5dffba5ea5442cc63fab97864
Owen Rees <owenrees82@gmail.com>
2023-08-23 21:44:38 +0200

    project scaffolding

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
+const withNextIntl = require("next-intl/plugin")("./i18n.ts");
+
 module.exports = nextConfig
+module.exports = withBundleAnalyzer(withNextIntl({}));
