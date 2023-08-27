f721bc3348bca2c755df920546e77c3712fb864f
Owen Rees <owenrees82@gmail.com>
2023-08-27 22:08:17 +0200

    board bg colour

--- client/app/[lang]/chessboard/Board.tsx
+++ client/app/[lang]/chessboard/Board.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useLayoutEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import LichessPgnViewer from "lichess-pgn-viewer";
 import "@/scss/main.scss";
 
@@ -8,13 +8,13 @@ export default function Board(props: any) {
   const pgn = props.children;
   const boardContainerRef = useRef<HTMLDivElement | null>(null);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (boardContainerRef.current) {
       LichessPgnViewer(boardContainerRef.current, {
         pgn: pgn,
       });
     }
-  });
+  }, [pgn]);
 
   return <div ref={boardContainerRef}></div>;
 }
